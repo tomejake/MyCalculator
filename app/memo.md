edf7bd8c20fc0a92e217e58035c7419648638808
tomejake <tomejake22@gmail.com>
2022-08-11 11:50:54 +0900

    UI 완성

---
+++ app/memo.md
@@ -0,0 +1,13 @@
+### android:orientation="" : 정렬
+- LinearLayout 이 중첩되어 있다면 각각의 Layout 의 방향을 지정해줘야함
+
+### layout_weight="": 가중치 설정
+- 예시) 테이블 느낌
+1 1 1 1
+2 2
+4
+
+### android:onClick: 코드가 복잡해서져서 더는 사용하지 않음
+
+### Toast 사용법
+- Toast.makeText(this, "내용", Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.mycalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // view: 이벤트가 일어났을때 버튼의 역할을 함
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
     }
 }
